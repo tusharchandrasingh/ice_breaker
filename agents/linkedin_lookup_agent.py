139e53029ae6e9a1d565b8ec1bf26b56c2e89bad
Tushar Singh <tusharchandrasingh@gmail.com>
2023-09-23 16:19:22 -0700

    implemented linkedin agent

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -7,10 +7,6 @@ from tools.tools import get_profile_url
 
 
 def lookup(name: str) -> str:
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    template = """given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
-                          Your answer should contain only a URL"""
-
     tools_for_agent = [
         Tool(
             name="Crawl Google 4 linkedin profile page",
@@ -19,14 +15,20 @@ def lookup(name: str) -> str:
         )
     ]
 
+    template = """given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
+                          Your answer should contain only a URL"""
+
+    prompt_template = PromptTemplate(
+        input_variables=["name_of_person"], template=template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
     agent = initialize_agent(
         tools=tools_for_agent,
         llm=llm,
         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True,
-    )
-    prompt_template = PromptTemplate(
-        template=template, input_variables=["name_of_person"]
     )
 
     linked_profile_url = agent.run(prompt_template.format_prompt(name_of_person=name))
